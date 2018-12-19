76aabe5e38373d10f9872df428fa0c3608c6ca86
LeilaNa <44800914+LeilaNa@users.noreply.github.com>
2018-12-19 11:33:33 +0400

    Add files via upload

---
+++ accordion.js
@@ -0,0 +1,34 @@
+$(document).ready(
+    function(){
+        $("li").hide();
+    })
+$(document).ready(function(){
+    $(".first").click(function(){
+        $("li").hide();
+      $(".first li").slideToggle();
+      });
+      });
+  $(document).ready(function(){
+    $(".second").click(function(){
+        $("li").hide();
+      $(".second li").slideToggle();
+    });
+      });
+  $(document).ready(function(){
+    $(".third").click(function(){
+        $("li").hide();
+      $(".third li").slideToggle();
+    });
+     });
+  $(document).ready(function(){
+    $(".fourth").click(function(){
+        $("li").hide();
+      $(".fourth li").slideToggle();
+    });
+    });
+  $(document).ready(function(){
+    $(".fifth").click(function(){
+        $("li").hide();
+      $(".fifth li").slideToggle();
+    });
+    });
